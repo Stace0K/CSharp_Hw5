ed8b4e021b764f6c9a8634f5eb1aad3b8f8decb0
anstice <ko.an.2018@yandex.ru>
2023-03-01 22:27:13 +0300

    task 38 ('halfway') completed

---
+++ Task38/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+void CreateArray(double[] array)
+{
+    int arr = array.Length;
+    for (int i = 0; i < arr; i++)
+    {
+        array[i] = new Random().Next(1, 9) + new Random().NextDouble();
+    }
+}
+
+void PrintArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+double Max(double[] array)
+{
+    double max = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > max) max = array[i];
+    }
+    return max;
+}
+
+double Min(double[] array)
+{
+    double min = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] < min) min = array[i];
+    }
+    return min;
+}
+
+
+double[] arr = new double[11];
+CreateArray(arr);
+PrintArray(arr);
+double max = Max(arr);
+double min = Min(arr);
+Console.WriteLine($"{max} - {min} = {max - min}");
